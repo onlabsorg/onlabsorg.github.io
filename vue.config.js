31faeb4180fdc3dd81da718001ae3e0cbe034d7e
Marcello Del Buono <m.delbuono@gmail.com>
2022-04-01 22:49:16 +0200

    Changed app title

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
 const pathlib = require('path');
 
 module.exports = {
+
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'OnLabs.org',
+        },
+    },
+
     outputDir: pathlib.resolve(__dirname, "docs"),
+
     transpileDependencies: [
         'vuetify'
     ]
